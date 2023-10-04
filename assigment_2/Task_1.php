b86cd0da02ac415e6025a27c3b50e76bf8289531
Md. Shahariar Rahman <99550336+shahariarr@users.noreply.github.com>
2023-10-04 18:49:53 +0600

    Module_4_Assignment

--- assigment_2/Task_1.php
+++ assigment_2/Task_1.php
@@ -6,10 +6,12 @@ function printEvenNumbersWhile($start, $end, $step) // function that prints even
         if ($i % 2 === 0) { // if $i is divisible by 2, print $i
             echo $i . " ";
         }
-        $i += $step; // increment $i by $step
+        $i += $step; // increment $i by $step 
+        
     }
 }
 
 // Call the function
-printEvenNumbersWhile(1, 20, 2); // prints even numbers from 1 to 20, incrementing by 2
+$result=printEvenNumbersWhile(1, 20, 2); // prints even numbers from 1 to 20, incrementing by 2
+
 ?>
